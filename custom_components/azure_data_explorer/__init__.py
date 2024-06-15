0ad512179cd57a9569984d6dc67989221529a3ba
kaareseras <kaare@seras.dk>
2024-06-15 15:52:37 +0200

    updated to working code

--- custom_components/azure_data_explorer/__init__.py
+++ custom_components/azure_data_explorer/__init__.py
@@ -65,7 +65,7 @@ async def async_setup(hass: HomeAssistant, yaml_config: ConfigType) -> bool:
     """
 
     hass.data.setdefault(DOMAIN, {DATA_FILTER: FILTER_SCHEMA({})})
-    if DOMAIN not in yaml_config:
+    if DOMAIN in yaml_config:
         hass.data[DOMAIN][DATA_FILTER] = yaml_config[DOMAIN].pop(CONF_FILTER)
 
     return True
